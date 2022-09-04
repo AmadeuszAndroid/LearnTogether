dcc351bf27b7faf414721778d85b35bcc411530f
Amadeusz Lisiecki <amadeusz.lisiecki.1993@gmail.com>
2022-09-04 20:32:47 +0200

    Utworzony tekst 3.

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,8 +44,20 @@ fun Tutorial() {
             contentDescription = null
         )
         Text(
-            text = stringResource(R.string.title),
+            text = stringResource(R.string.text_1),
             fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.text_2),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+        )
+        Text(
+            text = stringResource(R.string.text_3),
+            textAlign = TextAlign.Justify,
             modifier = Modifier
                 .padding(16.dp)
         )
