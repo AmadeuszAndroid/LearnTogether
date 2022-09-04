c605900ff8020942af4f49d8a97e6e47d230af72
Amadeusz Lisiecki <amadeusz.lisiecki.1993@gmail.com>
2022-09-04 18:14:27 +0200

    Ustawiony obraz.

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.learntogether
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
@@ -31,7 +35,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    val painter = painterResource(R.drawable.bg_compose_background)
+    Box {
+        Image(
+            painter = painter,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+
+        //Text(text = "Hello $name!")
+    }
 }
 
 @Preview(showBackground = true)
