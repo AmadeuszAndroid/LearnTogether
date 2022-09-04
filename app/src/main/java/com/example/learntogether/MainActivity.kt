9f1da2f3a3577efe4a4d515a6f416b0da7ce7e44
Amadeusz Lisiecki <amadeusz.lisiecki.1993@gmail.com>
2022-09-04 19:58:28 +0200

    Utworzony tekst 1.

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -4,16 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Tutorial()
                 }
             }
         }
@@ -34,17 +35,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
+fun Tutorial() {
     val painter = painterResource(R.drawable.bg_compose_background)
-    Box {
+    Column {
         Image(
             painter = painter,
-            contentDescription = null,
-            modifier = Modifier
-                .fillMaxWidth()
+            contentDescription = null
         )
-
-        //Text(text = "Hello $name!")
+        Text(
+            text = stringResource(R.string.title),
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
     }
 }
 
@@ -52,6 +55,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     LearnTogetherTheme {
-        Greeting("Android")
+        Tutorial()
     }
 }
